37eba9ea5d2874953b50dd30fb2337b3d859e928
gavhart <har19076@byui.edu>
2024-01-13 14:16:24 -0700

    something isnt right

---
+++ lesson 1/src/typescript/script.js
@@ -0,0 +1,6 @@
+function changeText() {
+    var textElement = document.getElementById('myText');
+    if (textElement) {
+        textElement.innerHTML = 'Text Changed!';
+    }
+}
---
+++ lesson 1/src/typescript/script.ts
@@ -0,0 +1,8 @@
+function changeText(): void {
+  const textElement = document.getElementById('myText');
+  if (textElement) {
+      textElement.innerHTML = 'Text Changed!';
+  }
+}
+
+
